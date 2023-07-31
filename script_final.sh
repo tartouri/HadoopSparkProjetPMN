274191a9a31ff64975d68f0be6d239f89ff36011
tartouri <81759679+tartouri@users.noreply.github.com>
2023-07-31 17:55:13 +0200

    Add files via upload

---
+++ script_final.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+chmod +x verify_input.sh
+./verify_input.sh
+
+chmod +x copie_data_input.sh
+./copie_data_input.sh
+
+chmod +x copie_data_tmp.sh
+./copie_data_tmp.sh
+
+chmod +x delete_tmp.sh
+./delete_tmp.sh
+
+chmod +x delete_content_input.sh
+./delete_content_input.sh
+
+chmod +x run_spark.sh
+./run_spark.sh
+
